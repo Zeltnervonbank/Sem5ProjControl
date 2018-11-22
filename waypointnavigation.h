4ad7c7a1e8dcd1a4663ebbd1b23b19beee11c66c
Andreas Zeltner <aszeltner95@gmail.com>
2018-11-22 16:36:55 +0100

    Created initial structure for class

--- waypointnavigation.h
+++ waypointnavigation.h
@@ -1,11 +1,24 @@
 #ifndef WAYPOINTNAVIGATION_H
 #define WAYPOINTNAVIGATION_H
 
+#include <gazebo/gazebo_client.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/transport/transport.hh>
+
+#include <queue>
 
 class WaypointNavigation
-{
+{    
 public:
+    struct Waypoint
+    {
+        double x;
+        double y;
+    };
+
     WaypointNavigation();
+    static std::queue<Waypoint> waypoints;
+
 };
 
 #endif // WAYPOINTNAVIGATION_H
